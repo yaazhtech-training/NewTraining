c308adf644d556a08aa25b1e416a5f70508fa952
jenita <131258921+Yaazhtec@users.noreply.github.com>
2024-04-03 11:23:09 +0530

    commit new

--- src/main/java/com/yaazhtech/employeeData/data/Exception.java
+++ src/main/java/com/yaazhtech/employeeData/data/Exception.java
@@ -2,18 +2,16 @@ package com.yaazhtech.employeeData.data;
 
 public class Exception {
 
-
-
     public static void main(String args[]){
         int i=9;
-        int j=3;
-        int  k=0;
+        int j=0;
+        int k=0;
         try {
             k = i / j;
         }
         catch (java.lang.Exception e) {
             System.out.println(k);
         }
-        System.out.println("bye");
+        System.out.println("hi");
     }
 }
